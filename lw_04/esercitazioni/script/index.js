314ee920fba1441b7edbd6c7cf6e40bb4f413d99
feiandart <fabriziafisichella@gmail.com>
2024-04-05 17:53:32 +0200

    Added first exercise

---
+++ lw_04/esercitazioni/script/index.js
@@ -0,0 +1,11 @@
+
+// ----------- ESERCIZIO #1 ----------- //
+
+let userBirth = prompt ("Inserisci la tua età:");
+
+if (userBirth === "01/01/1990") {
+    console.log("Buon compleanno! 🎁 ");
+} else {
+    console.log("Non è ancora il tuo compleanno!");
+}
+
