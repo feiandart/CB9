98ea45bce9cffb9f12708d8026b33fee2f8d1279
feiandart <fabriziafisichella@gmail.com>
2024-04-05 18:49:31 +0200

    Added second exercise + minor change in first exercise's comment

--- lw_04/esercitazioni/script/index.js
+++ lw_04/esercitazioni/script/index.js
@@ -1,5 +1,6 @@
 
 // ----------- ESERCIZIO #1 ----------- //
+// già incluso di esercizio avanzato #3
 
 let userBirth = prompt ("Inserisci la tua età:");
 
@@ -7,5 +8,39 @@ if (userBirth === "01/01/1990") {
     console.log("Buon compleanno! 🎁 ");
 } else {
     console.log("Non è ancora il tuo compleanno!");
-}
+} 
 
+
+
+// ----------- ESERCIZIO #2 ----------- //
+// avrei potuto farlo con una sola costante, ma volevo vedere se riuscivo a combinarne due...
+
+const dailyHours = [
+    "06.00",
+    "08.00",
+    "10.00",
+    "12.00",
+    "14.00",
+    "16.00",
+    "18.00",
+    "20.00",
+    "22.00",
+    "24.00"
+]
+
+const toDoList = [
+    "Ora di svegliarsi!",
+    "Magari anche fare colazione?",
+    "Potrebbe essere utile fare esercizio fisico.",
+    "Oppure potrei riposare di nuovo...",
+    "E se invece cucinassi?",
+    "Alla fine ho ordinato al take-away...",
+    "Dovrei davvero mettermi a lavorare, adesso.",
+    "Ma non è troppo tardi? Ormai meglio cenare!",
+    "Sono stanca, a questo punto potrei guardare un film...",
+    "Ora di andare a dormire, buonanotte!"
+]
+
+for (let list = 0; list < dailyHours.length; list++) {
+    console.log(dailyHours[list] + ") " + toDoList[list]);
+}
