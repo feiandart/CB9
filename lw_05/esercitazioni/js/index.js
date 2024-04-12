e9633aa262670f15e7903f1ac13256e0033b95af
feiandart <fabriziafisichella@gmail.com>
2024-04-12 19:59:30 +0200

    Second exercise addes

--- lw_05/esercitazioni/js/index.js
+++ lw_05/esercitazioni/js/index.js
@@ -1,6 +1,6 @@
 // !! ----- ESERCIZIO #1 ----- !! //
 
-function vowelCount(yourSentence) {
+/* function vowelCount(yourSentence) {
     let vocals = { a: 0, e: 0, i: 0, o: 0, u: 0 };
     for (let characters of yourSentence) {
         if ('aeiouàèéìòù'.includes(characters)) {
@@ -13,5 +13,21 @@ function vowelCount(yourSentence) {
 
 let yourSentence = prompt("Dimmi una frase e ti conto le vocali!");
 let countResult = vowelCount(yourSentence);
-alert(countResult);
+alert(countResult); */
 
+
+
+// !! ----- ESERCIZIO #2 ----- !! //
+
+/* function maxNum(totalNums) {
+    for(num in totalNums) {
+        return Math.max(...totalNums.map(totalNums => totalNums));
+    }
+}
+
+const num1 = prompt("Inserisci il primo numero:");
+const num2 = prompt("Inserisci il secondo numero:");
+const num3 = prompt("Inserisci il terzo numero:");
+const totalNums = [num1, num2, num3]
+
+console.log("Il numero più alto è:", maxNum(totalNums)); */
