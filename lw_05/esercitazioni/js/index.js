3ab9d31f85a6bde841f1ec4bffd84e42b5dec122
feiandart <fabriziafisichella@gmail.com>
2024-04-13 19:22:48 +0200

    Advanced Exercise #1

--- lw_05/esercitazioni/js/index.js
+++ lw_05/esercitazioni/js/index.js
@@ -29,5 +29,27 @@ const num1 = prompt("Inserisci il primo numero:");
 const num2 = prompt("Inserisci il secondo numero:");
 const num3 = prompt("Inserisci il terzo numero:");
 const totalNums = [num1, num2, num3]
+let result = maxNum(totalNums);
 
-console.log("Il numero più alto è:", maxNum(totalNums)); */
+console.log("Il numero più alto è:", result); */
+
+
+
+// !! ----- ESERCIZIO AVANZATO #1 ----- !! //
+
+/* function addSalary(selectSalary) {
+    const raiseSalary = selectSalary.map(person => {
+        return { ...person, salario: person.salario * 1.1 };
+    })
+    return raiseSalary;
+}
+
+const people = [
+    { nome: "Marco", salario: 2000 },
+    { nome: "Luca", salario: 4000 },
+    { nome: "Lia", salario: 2500 }
+];
+let selectSalary = people.filter(person => (person.salario < 3000));
+let updatedSalary = addSalary(selectSalary);
+
+console.log(updatedSalary); */
